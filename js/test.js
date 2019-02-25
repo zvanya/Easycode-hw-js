a3c26f0d467faa7ba1765641b3613410fe79b2f6
zvanya <zhakin.v@gmail.com>
2019-02-25 22:51:33 +0200

    modified test.js

--- js/test.js
+++ js/test.js
@@ -6,3 +6,21 @@ console.log(x(1)(7));
 
 const path = require('path');
 console.log(`path = ${path.resolve(__dirname)}`);
+
+
+class Planet {
+    constructor(name = "Mars") {
+        this.name = name;
+        
+        this.f1 = function() {
+            console.log(this.name);
+        }
+    }
+    
+    getName() {
+        return 'Planet name is ' + this.name;
+    }
+}
+
+const p1 = new Planet("earth");
+p1.f1();
